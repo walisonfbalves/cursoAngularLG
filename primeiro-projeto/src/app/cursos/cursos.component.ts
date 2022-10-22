dadfbed40b14f8534b43b565174e24e43ed37353
Walison Fernandes Barbosa Alves <walison.alves@mirante.net.br>
2022-10-22 17:08:34 -0300

    Introducao a Templates

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['HTMl','CSS', 'Java', 'Angular']
+
+  constructor() {
+
+    this.nomePortal = 'http://loiane.training';
+
+    for (let i=0; i<this.cursos.length; i++) {
+      let curso = this.cursos[i];
+    }
+  }
 
   ngOnInit(): void {
   }
