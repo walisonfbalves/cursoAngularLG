15037b01a56c8531330a173a309527a99d255f24
Walison Fernandes Barbosa Alves <walison.alves@mirante.net.br>
2022-12-24 21:07:50 -0300

    Adicionando nova pasta - diretivas

--- diretivas/diretivas/src/app/app.module.ts
+++ diretivas/diretivas/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DiretivaNgifComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
